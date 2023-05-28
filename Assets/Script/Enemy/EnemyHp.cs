0c1a6fc571806c0d62bdf6bde6b87a64922b5a29
mlpoo <113955016+mlpoo@users.noreply.github.com>
2023-05-29 07:14:03 +0900

    20230529a
    [TimeManager]
    - 변수 currentTime가 게임진행시간을 기록합니다.
    - 5분 후부터 변수overTime이 증가합니다. (이건 변수 더 안만들어도 되는데 편의성을 위해 만들었습니다. 변경하셔도 됩니다.)
    
    [ScoreManager]
    - 점수 기록을 합니다.
    
    [Player]
    - 충돌 감지를 합니다.
    - 플레이어의 hp를 관리합니다.
    
    **playerHp는 Player에서 관리하는 것이 더 편해서 삭제했습니다.**
    **trigger할 수있는 것이 monster밖에 없어서 별다른 조건을 걸지 않았습니다.**

--- Assets/Script/Enemy/EnemyHp.cs
+++ Assets/Script/Enemy/EnemyHp.cs
@@ -6,28 +6,11 @@ using UnityEngine;
 
 public class EnemyHp : MonoBehaviour
 {
-    private BoxCollider enemyTrigger;
     private float enemyHp;
 
-    // player의 trigger를 붙여준다.
-    [SerializeField]
-    private BoxCollider playerTrigger;
-    [SerializeField]
-    private float playerHp;
-    
-    private bool isTrigger = false;
     void Start()
     {
-        enemyTrigger = GetComponent<BoxCollider>();
         enemyHp = 100;
     }
-    private void OnTriggerEnter(Collider playerCollider)
-    {
-        isTrigger = true;
-    }
-
-    private void OnCollisionExit(Collision playerCollider)
-    {
-        isTrigger = false;
-    }
+    
 }
--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,13 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private BoxCollider Trigger;
-    private bool isTrigger = false;
+    private float hp;
+    
+    private bool isTrigger; // 다른 물체와 접촉(관통)하고 있는지 확인합니다.
+
     void Start()
     {
-        Trigger = GetComponent<BoxCollider>();
+        Trigger = GetComponent<BoxCollider>(); 
+        hp = 100.0f;
+        isTrigger = false;
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        isTrigger = true;
+        hp -= 20f;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isTrigger = false;
     }
 }
