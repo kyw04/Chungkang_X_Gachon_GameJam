1227cef45396460dff2eafe965af09be7dbeaaa3
kyw04 <85551017+kyw04@users.noreply.github.com>
2023-05-29 22:45:57 +0900

    무기추가

--- Assets/Script/Player/Rotation.cs
+++ Assets/Script/Player/Rotation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
+    public Transform weapon;
     private void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -13,7 +14,18 @@ public class Rotation : MonoBehaviour
             Vector3 point = hit.point;
             point.y = transform.position.y;
             transform.LookAt(point);
-            Debug.DrawRay(Camera.main.transform.position, ray.direction * 500, Color.red);
+            //Debug.DrawRay(Camera.main.transform.position, ray.direction * 500, Color.red);
+
+            //weapon.LookAt(transform.position);
+            //weapon.Rotate(90, 0, 0);
+            //if (point.x - transform.position.x >= 0)
+            //{
+            //    weapon.rotation = Quaternion.Euler(90, 0, 0);
+            //}
+            //else
+            //{
+            //    weapon.rotation = Quaternion.Euler(-90, 0, 0);
+            //}
         }
     }
 }
